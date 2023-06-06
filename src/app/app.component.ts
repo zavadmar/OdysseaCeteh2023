d8844e83071cca8a2898893edc421840d0308de9
mzavadil <martin.zavadil@profinit.eu>
2023-06-06 15:54:03 +0200

    Simulace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,86 @@ export class AppComponent {
       brick: 0,
     },
     points: 0,
+    stats: {
+      winPriceSum: 0,
+      incomeSum: {
+        money: 0,
+        wood: 0,
+        stone: 0,
+        brick: 0,
+      }
+    },
+    availableBuildings: [...buildingsList],
+    log: [] as string[],
+  },
+  {
+    name: "2",
+    buildings: [] as any[],
+    resources: {
+      money: 500,
+      wood: 0,
+      stone: 0,
+      brick: 0,
+    },
+    points: 0,
+    stats: {
+      winPriceSum: 0,
+      incomeSum: {
+        money: 0,
+        wood: 0,
+        stone: 0,
+        brick: 0,
+      }
+    },
+    availableBuildings: [...buildingsList],
+    log: [] as string[],
+  },
+  {
+    name: "3",
+    buildings: [] as any[],
+    resources: {
+      money: 500,
+      wood: 0,
+      stone: 0,
+      brick: 0,
+    },
+    points: 0,
+    stats: {
+      winPriceSum: 0,
+      incomeSum: {
+        money: 0,
+        wood: 0,
+        stone: 0,
+        brick: 0,
+      }
+    },
+    availableBuildings: [...buildingsList],
+    log: [] as string[],
+  },
+  {
+    name: "4",
+    buildings: [] as any[],
+    resources: {
+      money: 500,
+      wood: 0,
+      stone: 0,
+      brick: 0,
+    },
+    points: 0,
+    stats: {
+      winPriceSum: 0,
+      incomeSum: {
+        money: 0,
+        wood: 0,
+        stone: 0,
+        brick: 0,
+      }
+    },
+    availableBuildings: [...buildingsList],
+    log: [] as string[],
   }]
 
-  buildings = buildingsList;
+  //buildings = [...buildingsList];
 
   marketResources = [{
     name: "Dřevo",
@@ -43,11 +120,10 @@ export class AppComponent {
     discountPrice: 140,
   }];
 
-  log: string[] = [];
-
-  addPrice(team: any) {
-    team.resources.money += this.winPrice;
-    this.log.push("Výhra " + this.winPrice)
+  addPrice(team: any, winPrice: number) {
+    team.resources.money += winPrice;
+    team.log.push("Výhra " + winPrice)
+    team.stats.winPriceSum += winPrice;
   }
 
   addIncome(team: any) {
@@ -62,12 +138,16 @@ export class AppComponent {
       team.resources.brick += income?.brick ?? 0;
       team.resources.wood += income?.wood ?? 0;
       team.resources.stone += income?.stone ?? 0;
+      team.stats.incomeSum.money += income?.money ?? 0;
+      team.stats.incomeSum.brick += income?.brick ?? 0;
+      team.stats.incomeSum.wood += income?.wood ?? 0;
+      team.stats.incomeSum.stone += income?.stone ?? 0;
     });
-    this.log.push("Výnosy:");
-    this.log.push("- peníze: " + (team.resources.money - startMoney));
-    this.log.push("- dřevo: " + (team.resources.wood - startWood));
-    this.log.push("- kámen: " + (team.resources.stone - startStone));
-    this.log.push("- cihly: " + (team.resources.brick - startBrick));
+    team.log.push("Výnosy:");
+    team.log.push("- peníze: " + (team.resources.money - startMoney));
+    team.log.push("- dřevo: " + (team.resources.wood - startWood));
+    team.log.push("- kámen: " + (team.resources.stone - startStone));
+    team.log.push("- cihly: " + (team.resources.brick - startBrick));
   }
 
   buy(team: any, building: any) {
@@ -82,9 +162,9 @@ export class AppComponent {
         team.resources.wood -= building.price.wood;
         team.resources.brick -= building.price.brick;
         team.buildings.push(building);
-        this.buildings = this.buildings.filter(x => x.name !== building.name);
+        team.availableBuildings = team.availableBuildings.filter((x: any) => x.name !== building.name);
         team.points += this.countResourcesToMoney(building);
-        this.log.push("Nákup: " + building.name);
+        team.log.push("Nákup: " + building.name);
       }
   }
 
@@ -93,6 +173,13 @@ export class AppComponent {
     + building.price.wood * this.marketResources[0].price 
     + building.price.stone * this.marketResources[1].price 
     + building.price.brick * this.marketResources[2].price;
+  }
+
+  public countResourcesToMoneyWithDiscount(building: any) {
+    return building.price.money
+    + building.price.wood * this.marketResources[0].discountPrice 
+    + building.price.stone * this.marketResources[1].discountPrice 
+    + building.price.brick * this.marketResources[2].discountPrice;
   }
 
   buyResource(team: any, resource: any) {
@@ -106,16 +193,170 @@ export class AppComponent {
       team.resources.money -= price;
       switch(resource.propertyName) {
         case "wood": team.resources.wood++;
-          this.log.push("Nákup dřevo: 1 za " + price);
+          team.log.push("Nákup dřevo: 1 za " + price);
         break;
         case "stone": team.resources.stone++;
-          this.log.push("Nákup kámen: 1 za " + price);
+          team.log.push("Nákup kámen: 1 za " + price);
         break;
         case "brick": team.resources.brick++;
-        this.log.push("Nákup cihly: 1 za " + price);
+          team.log.push("Nákup cihly: 1 za " + price);
         break;
         default: break;
       }
     }
   }
+
+  private bigPrice = [800, 600, 400, 200];
+  private smallPrice = [400, 300, 200, 100];
+
+  prices = [
+    [
+      this.bigPrice,
+      this.smallPrice,
+    ],
+    [
+      this.bigPrice,
+    ],
+    [
+      this.bigPrice,
+      this.smallPrice,
+    ],
+    [
+      this.bigPrice,
+    ],
+    [
+      this.bigPrice,
+      this.smallPrice,
+    ],
+    [
+      this.bigPrice,
+    ],
+    [
+      this.bigPrice,
+      this.smallPrice,
+    ],
+    [
+      this.bigPrice,
+    ],
+    [
+      this.bigPrice,
+      this.smallPrice,
+    ],
+    [
+      this.bigPrice,
+    ]
+  ]
+
+  maxPrices = this.prices.map(x => [...x.map(y => [y[0]])]);
+  minPrices = this.prices.map(x => [...x.map(y => [y[3]])]);
+
+  simulateRandom(team: any, prices: number[][][]) {
+    prices.forEach((day, index) => {
+      team.log.push("------Den " + index + "-----");
+      day.forEach(dayPrice => {
+        this.addPrice(team, dayPrice[this.getRandomInt(dayPrice.length)])
+      });
+
+      this.buyAvailableBuilding(team);
+      if (team.buildings.some((x: any) => x.name === "Stavitel")) {
+        this.buyAvailableBuilding(team);
+      }
+      this.addIncome(team);
+    });
+  }
+
+  simulateTeams(random = false) {
+    const shuffledPrices = this.prices.map(x => x.map(y => this.shuffle(y)));
+    shuffledPrices.forEach((day, index) => {
+    this.teams.forEach((team, teamIndex) => {
+      team.log.push("------Den " + index + "-----");
+      day.forEach(dayPrice => {
+        this.addPrice(team, dayPrice[teamIndex])
+      });
+
+      this.buyAvailableBuilding(team, random);
+      if (team.buildings.some((x: any) => x.name === "Stavitel")) {
+        this.buyAvailableBuilding(team);
+      }
+      this.addIncome(team);
+    });
+    });
+  }
+
+  private shuffle(array: number[]) {
+    return array
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+  }
+
+  private buyAvailableBuilding(team: any, randomChoose = false) {
+    const available = team.availableBuildings.filter((x: any) => this.canBuy(team, x));
+      if (available.length > 0) {
+        let toBuild = null;
+        if (randomChoose) {
+          toBuild = available[this.getRandomInt(available.length)];
+        }
+        else {
+          toBuild = available.sort((a: any, b: any) => b.priority - a.priority)[0];
+        } 
+
+        for(let i = team.resources.wood; i < toBuild.price.wood; i++) {
+          this.buyResource(team, this.marketResources.find(x => x.propertyName === "wood"));
+        }
+        for(let i = team.resources.stone; i < toBuild.price.stone; i++) {
+          this.buyResource(team, this.marketResources.find(x => x.propertyName === "stone"));
+        }
+        for(let i = team.resources.brick; i < toBuild.price.brick; i++) {
+          this.buyResource(team, this.marketResources.find(x => x.propertyName === "brick"));
+        }
+        this.buy(team, toBuild);
+      }
+  }
+
+  private getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  public countIncomeRate(team: any, building: any) {
+    const price = this.countResourcesToMoney(building);
+    const income = building.incomeFn != null ? building.incomeFn(team) : building.income;
+    const incomeMoney = income.money 
+    + (income?.wood ?? 0) * (this.marketResources.find(x => x.propertyName === "wood")?.price ?? 0)
+    + (income?.stone ?? 0) * (this.marketResources.find(x => x.propertyName === "stone")?.price ?? 0)
+    + (income?.brick ?? 0) * (this.marketResources.find(x => x.propertyName === "brick")?.price ?? 0);
+    return incomeMoney / price;
+  }
+
+  public countIncomeDiscountRate(team: any, building: any) {
+    const price = this.countResourcesToMoneyWithDiscount(building);
+    const income = building.incomeFn != null ? building.incomeFn(team) : building.income;
+    const incomeMoney = income.money 
+    + (income?.wood ?? 0) * (this.marketResources.find(x => x.propertyName === "wood")?.price ?? 0)
+    + (income?.stone ?? 0) * (this.marketResources.find(x => x.propertyName === "stone")?.price ?? 0)
+    + (income?.brick ?? 0) * (this.marketResources.find(x => x.propertyName === "brick")?.price ?? 0);
+    return incomeMoney / price;
+  }
+
+  private canBuy(team: any, building: any): boolean {
+    const needWood = Math.max(0, building.price.wood - team.resources.wood);
+    const needStone = Math.max(0, building.price.stone - team.resources.stone);
+    const needBrick = Math.max(0, building.price.brick - team.resources.brick);
+
+    let needMoney = 0;
+    if (team.buildings.some((x: any) => x.hasDiscount)) {
+      needMoney = 
+        needWood * this.marketResources[0].discountPrice
+        + needStone * this.marketResources[1].discountPrice
+        + needBrick * this.marketResources[2].discountPrice;
+    }
+    else {
+      needMoney = 
+        needWood * this.marketResources[0].price
+        + needStone * this.marketResources[1].price
+        + needBrick * this.marketResources[2].price;
+    }
+    return team.resources.money > needMoney;
+  }
 }
+
--- src/app/resources/buildings.ts
+++ src/app/resources/buildings.ts
@@ -1,48 +1,57 @@
 export const buildingsList = [{
     name: "Pila",
     income: { wood: 1, stone: 0, brick: 0, money: 0 },
-    price: { wood: 2, stone: 2, brick: 2, money: 0 }
+    price: { wood: 2, stone: 2, brick: 1, money: 0 },
+    priority: 100
   },
   {
     name: "Kamenolom",
     income: { wood: 0, stone: 1, brick: 0, money: 0 },
-    price: { wood: 3, stone: 2, brick: 3, money: 0 }
+    price: { wood: 3, stone: 2, brick: 3, money: 0 },
+    priority: 700
   },
   {
     name: "Cihelna",
     income: { wood: 0, stone: 0, brick: 1, money: 0 },
-    price: { wood: 5, stone: 3, brick: 3, money: 0 }
+    price: { wood: 6, stone: 3, brick: 4, money: 0 },
+    priority: 800,
   },
   {
     name: "Tržiště",
     hasDiscount: true,
     income: { wood: 0, stone: 0, brick: 0, money: 0 },
-    price: { wood: 3, stone: 3, brick: 5, money: 0 }
+    price: { wood: 3, stone: 3, brick: 5, money: 0 },
+    priority: 600,
   },
   {
     name: "Hokynářství",
     income: { wood: 0, stone: 0, brick: 0, money: 100 },
-    price: { wood: 4, stone: 3, brick: 4, money: 0 }
+    price: { wood: 4, stone: 3, brick: 3, money: 0 },
+    priority: 50,
   },
   {
     name: "Banka",
     income: { wood: 0, stone: 0, brick: 0, money: 300 },
-    price: { wood: 6, stone: 5, brick: 8, money: 0 }
+    price: { wood: 6, stone: 5, brick: 8, money: 0 },
+    priority: 900,
   },
   {
     name: "Pošta",
     income: { wood: 0, stone: 0, brick: 0, money: 50 },
-    price: { wood: 5, stone: 3, brick: 2, money: 0 }
+    price: { wood: 3, stone: 1, brick: 2, money: 0 },
+    priority: 30,
   },
   {
     name: "Hotel",
     income: { wood: 0, stone: 0, brick: 0, money: 200 },
-    price: { wood: 3, stone: 6, brick: 5, money: 0 }
+    price: { wood: 3, stone: 6, brick: 6, money: 0 },
+    priority: 500,
   },
   {
     name: "Saloon",
     income: { wood: 0, stone: 0, brick: 1, money: 0 },
-    price: { wood: 6, stone: 3, brick: 4, money: 0 }
+    price: { wood: 6, stone: 3, brick: 4, money: 0 },
+    priority: 300,
   },
   {
     name: "Herna*",
@@ -58,7 +67,8 @@ export const buildingsList = [{
         }
         return;
     },
-    price: { wood: 2, stone: 3, brick: 6, money: 0 }
+    price: { wood: 2, stone: 3, brick: 4, money: 0 },
+    priority: 200,
   },
   {
     name: "Šerif*",
@@ -67,7 +77,38 @@ export const buildingsList = [{
         const moneyBuildingsCount = team.buildings.filter((x: any) => (x.income?.money ?? 0) > 0).length;
         return { money: moneyBuildingsCount * 50 }
     },
-    price: { wood: 5, stone: 7, brick: 3, money: 0 }
+    price: { wood: 5, stone: 7, brick: 3, money: 0 },
+    priority: 400,
+  },
+  {
+    name: "Stavitel",
+    income: { wood: 0, stone: 0, brick: 0, money: 0 },
+    price: { wood: 2, stone: 1, brick: 3, money: 0 },
+    priority: 650,
+  },
+  {
+    name: "Nádraží",
+    income: { wood: 0, stone: 0, brick: 0, money: 150 },
+    price: { wood: 5, stone: 3, brick: 6, money: 0 },
+    priority: 100,
+  },
+  {
+    name: "Kovárna",
+    income: { wood: 0, stone: 0, brick: 0, money: 250 },
+    price: { wood: 5, stone: 7, brick: 6, money: 0 },
+    priority: 600,
+  },
+  {
+    name: "Holič",
+    income: { wood: 0, stone: 0, brick: 0, money: 80 },
+    price: { wood: 5, stone: 2, brick: 2, money: 0 },
+    priority: 200,
+  },
+  {
+    name: "Hrobník",
+    income: { wood: 0, stone: 0, brick: 0, money: 120 },
+    price: { wood: 3, stone: 4, brick: 4, money: 0 },
+    priority: 200,
   },
 ]
 
